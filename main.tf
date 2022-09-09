5ab4f052a5bd9a4f942594efaf467a1f1bdf166d
Matheus Tanure <matheus.tanure@iteris.com.br>
2022-09-09 10:16:32 -0300

    feat: pipeline pr

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
-provider "null" {
-  version = "2.1.0"
-}
-
 terraform {
-  required_version = "1.2.6"
+  required_version = ">= 0.13"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "2.49.0"
+    null = {
+      source = "hashicorp/null"
     }
   }
 }
